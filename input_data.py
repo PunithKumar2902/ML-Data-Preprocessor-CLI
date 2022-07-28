7b21a1c9636d467040cd6c07a96e9a241f22aee0
Punith Kumar <66864406+PunithKumar2902@users.noreply.github.com>
2022-07-28 23:17:52 +0530

    takes the input dataset from command line

---
+++ input_data.py
@@ -0,0 +1,43 @@
+from os import path
+import sys
+import pandas as pd
+
+class Data_input:
+
+    # all extensions supported by this project.
+    accepted_extensions=['.csv']
+    
+    # function to convert all the column names of any specific dataset into lowercase.
+    def to_lower_case(data):
+
+        for column in data.columns.values:
+
+            data.replace(columns={column : column.lower()},inplace = True)
+
+        return data
+
+    # function that takes any dataset from the input file and convert it into DataFrame.
+    def inputfunction(self):
+        
+        try:
+            
+            filename,extension= path.splittext(sys.argv[1])
+
+            if filename=="":
+                raise SystemExit("provide the DATASET name (with extension).")
+            
+            if extension not in self.accepted_extensions:
+                raise SystemExit("This file extension is not supported.")
+
+        except IndexError:
+            raise SystemExit("provide the DATASET name (with extension).")
+
+        try:
+            data = pd.read_csv(filename+extension)
+
+        except FileNotFoundError:
+            raise SystemExit("File doesn't exist")
+
+        data = self.to_lower_case(data)
+
+        return data
